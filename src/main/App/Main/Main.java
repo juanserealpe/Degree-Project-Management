54415df0b57d8bfa0706a6d91cb59b193eaf30bb
PonceAlex203 <poncealex203@gmail.com>
2025-09-16 22:41:04 -0500

    PrototypeV2
    DataBase
    Model

--- src/main/App/Main/Main.java
+++ src/main/App/Main/Main.java
@@ -1,9 +1,25 @@
 package Main;
 
+import DataBase.DbConnection;
 import javafx.application.Application;
+
+import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Application.launch(App.class, args);
+        //Application.launch(App.class, args);
+        try {
+            Connection conn = DbConnection.getConnection();
+            if (conn != null) {
+                System.out.println("Conexión establecida correctamente");
+            } else {
+                System.out.println("No se pudo establecer la conexión");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DbConnection.closeConnection();
+        }
     }
 }
