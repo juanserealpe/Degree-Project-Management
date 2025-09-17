d0119bdcccafb9d001a4e18c1df0c43dcd0705ec
juanserealpe <juansevarona@hotmail.com>
2025-09-16 21:31:12 -0500

    Listo merge

--- src/main/App/Main/Main.java
+++ src/main/App/Main/Main.java
@@ -1,24 +1,9 @@
 package Main;
 
-import DataBase.DbConnection;
 import javafx.application.Application;
-
-import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        try {
-            Connection conn = DbConnection.getConnection();
-            if (conn != null) {
-                System.out.println("Conexión establecida correctamente");
-            } else {
-                System.out.println("No se pudo establecer la conexión");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            DbConnection.closeConnection();
-        }
-       // Application.launch(App.class, args);
+        Application.launch(App.class, args);
     }
 }
