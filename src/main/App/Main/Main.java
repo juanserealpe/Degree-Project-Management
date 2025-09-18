77db76fb42c42ccda953173f7dd61dc0cbfc0a33
JhersonCastro <142602493+JhersonCastro@users.noreply.github.com>
2025-09-17 19:09:32 -0500

    Configuración de proyecto actualizada

--- src/main/App/Main/Main.java
+++
@@ -1,9 +0,0 @@
-package Main;
-
-import javafx.application.Application;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        Application.launch(App.class, args);
-    }
-}
