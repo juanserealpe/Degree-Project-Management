0e53caba4ed95ae9fcc4de4b0f6be734d1816752
juanserealpe <juansevarona@hotmail.com>
2025-09-23 07:23:09 -0500

    feat: agregar RegisterController (en progreso)
    Se agrega la clase RegisterController para manejar la lógica de registro de usuarios.
    Actualmente está en progreso; faltan implementaciones de validación y conexión con servicios.

--- src/main/App/Main/App.java
+++ src/main/App/Main/App.java
@@ -1,15 +1,21 @@
 package Main;
 
+import DataBase.DbConnection;
+import Services.ServiceFactory;
 import Utilities.WindowManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.Connection;
+
 public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Connection connection = DbConnection.getConnection();
+        ServiceFactory factory = new ServiceFactory(connection);
         FXMLLoader loader = new FXMLLoader(
                 getClass().getResource("/views/AuthViews/LoginView.fxml")
         );
--- src/main/App/Main/Main.java
+++ src/main/App/Main/Main.java
@@ -1,23 +1,7 @@
 package Main;
 
-import DataBase.DbConnection;
-import Dtos.UserRegisterDTO;
-import Enums.EnumProgram;
-import Enums.EnumRole;
-import Interfaces.IAuthService;
-import Models.Account;
-import Models.User;
-import Repositories.CredentialRepository;
-import Services.ServiceFactory;
-import Services.UserRegisterServices;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.List;
-
 public class Main {
-    public static void main(String[] args) throws Exception {
-
+    public static void main(String[] args) {
     }
 
 }
