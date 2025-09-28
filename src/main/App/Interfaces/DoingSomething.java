cba1c7fe6f3d651829fbfbc3fb660059b2d6ab31
JhersonCastro <142602493+JhersonCastro@users.noreply.github.com>
2025-09-27 20:51:05 -0500

    Add a FunctionalInterface DoingSomething, these work as a helper to send a func as parameter Session now is a real singleton

---
+++ src/main/App/Interfaces/DoingSomething.java
@@ -0,0 +1,5 @@
+package Interfaces;
+@FunctionalInterface
+public interface DoingSomething {
+    void apply(Object obj);
+}
--- src/main/App/Models/Session.java
+++ src/main/App/Models/Session.java
@@ -3,27 +3,34 @@ package Models;
 import Enums.EnumRole;
 import Utilities.Logger;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Session {
-
-    private static String email;
-    private static List<EnumRole> roles;
+    private static Session instance;
+    private String email;
+    private List<EnumRole> roles;
 
     public Session() {}
+    public static Session getInstance(){
+        if(instance == null){ instance = new Session();
+            instance.roles = new ArrayList<>();
 
-    public static String getEmail() {return email;}
-
-    public static void setEmail(String email) {Session.email = email;}
-
-    public static List<EnumRole> getRoles() {return roles;}
-    public static void logOut(){
-        Session.roles.clear();
-        Session.email = "";
+        }
+        return instance;
     }
-    public static void setRoles(List<EnumRole> roles) {
+    public String getEmail() {return email;}
+
+    public void setEmail(String email) {instance.email = email;}
+
+    public List<EnumRole> getRoles() {return roles;}
+    public void logOut(){
+        instance.roles.clear();
+        instance.email = "";
+    }
+    public void setRoles(List<EnumRole> roles) {
         Logger.info(Session.class, "setRoles: roles " + roles);
-        Session.roles = roles;
+        instance.roles = roles;
     }
 
     @Override
